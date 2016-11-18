e59003f6dc7c40da6af1dc1c9ad8964c9e3f99ec
Sunil <sonuame@aol.com>
2016-11-18 16:00:07 +0530

    Initial Commit
    Signed-off-by: Sunil <sonuame@aol.com>

---
+++ client_a.js
@@ -0,0 +1,26 @@
+﻿var thing = require('./app.js');
+
+
+var cli = process.argv;
+var Device = thing.Device;
+
+Device.ClientID = 2102;
+Device.Name = "TBDAutomations_Location_Bangalore";
+Device.PublishTopic = "register_swipe/log";
+Device.Payload.deviceid = Device.ClientID;
+Device.Payload.direction = "out";
+Device.Payload.empid = "B113";
+
+
+thing.initPayload(Device, cli);
+var device = thing.Connect();
+//thing.PullSubscriptions(device);
+
+device.publish(Device.PublishTopic, JSON.stringify(Device.Payload), { Qos : 0 }, function (e) {
+    console.log("Attendance Logged");
+    setTimeout(function () {
+        process.exit(-1);
+    }, 2000)
+});
+
+
---
+++ client_b.js
@@ -0,0 +1,21 @@
+﻿var thing = require('./app.js');
+
+var cli = process.argv;
+var Device = thing.Device;
+
+Device.ClientID = 2034;
+Device.Name = "TBDAutomations_Location_Mumbai";
+Device.PublishTopic = "register_swipe/log";
+Device.Payload.deviceid = Device.ClientID;
+Device.Payload.direction = "out";
+Device.Payload.empid = "B113";
+Device.Payload.timestamp = "";
+
+
+thing.initPayload(Device, cli);
+var device = thing.Connect();
+//thing.PullSubscriptions(device);
+
+device.publish(Device.PublishTopic, JSON.stringify(Device.Payload), { Qos : 0 }, function (e) {
+    console.log("Attendance Logged");
+});
