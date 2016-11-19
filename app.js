db09c4b51bab945bc53998842c0c39eaa037ad9d
Sunil <sonuame@aol.com>
2016-11-20 05:08:20 +0530

    Initial Sync

---
+++ app.js
@@ -0,0 +1,75 @@
+﻿var awsIot = require('aws-iot-device-sdk');
+var moment = require('moment');
+
+exports.Device = {
+    ClientID : 2102,
+    Name : undefined,
+    SubscriptionTopics : [],
+    DeviceReady : false,
+    PublishTopic : 'register_swipe/log',
+    Payload : {
+        deviceid: 0,
+        direction : undefined,
+        empid : undefined,
+        timestamp : new moment().format("HH:mm:ss")
+    }
+}
+
+
+exports.initPayload = function (Device, cli)
+{
+    if (cli.indexOf("-e") > -1 && cli.indexOf("-d") > -1) {
+        Device.Payload.direction = cli[cli.indexOf("-d") + 1].toUpperCase();
+        Device.Payload.empid = cli[cli.indexOf("-e") + 1].toUpperCase();
+        Device.Payload.timestamp = new moment().format("HH:mm:ss");
+    }
+}
+
+
+exports.Connect = function() {
+    console.log("Connecting...");
+    var device = awsIot.device({
+        "host": "a20jrkqareb4mh.iot.us-west-2.amazonaws.com",
+        "port": 8883,
+        "clientId": exports.Device.Name + "_" + exports.Device.ClientID,
+        "thingName": exports.Device.Name,
+        "caCert": "certs/root-CA.crt",
+        "clientCert": "certs/90c78d38ea-certificate.pem.crt",
+        "privateKey": "certs/90c78d38ea-private.pem.key"
+    });
+
+    device.on('connect', function () {
+        console.log('Device Connected to IOT');
+        exports.Device.DeviceReady = true;
+    });
+    return device;
+};
+
+exports.PullSubscriptions = function (_device) {
+    var mysql = require('mysql');
+    var pool = mysql.createPool({
+        host     : 'tbdautomations-hurestic.co9mgkul9wut.us-west-2.rds.amazonaws.com',
+        user     : 'tbdautomations',
+        password : 'Atmega328',
+        database : 'hurestic'
+    });
+    
+
+    console.log("Connecting to Database Server");
+    pool.getConnection(function (err, connection) {
+        console.log("Connected! Pulling Topics...");
+        connection.query('SELECT * from mqtt_subscription where deviceid=' + exports.Device.ClientID, function (err, rows) {
+            var interval = setInterval(function () {
+                if (exports.Device.DeviceReady) {
+                    clearInterval(interval);
+                    for (i = 0; i < rows.length; i++) {
+                        exports.Device.SubscriptionTopics.push(rows[i].topic_address);
+                        _device.subscribe(rows[i].topic_address);
+                    }
+                }
+            }, 10);
+            connection.release();
+            console.log("Additional Topics Subscribed");
+        });
+    });
+}
---
+++ lambda_handler.js
@@ -0,0 +1,34 @@
+﻿var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'tbdautomations-hurestic.co9mgkul9wut.us-west-2.rds.amazonaws.com',
+    user: 'tbdautomations',
+    password: 'Atmega328',
+    database: 'hurestic'
+});
+
+exports.handler = function (event, context) {
+    
+    var eventText = JSON.stringify(event, null, 2);
+    console.log("Received event:", eventText);
+    
+    pool.getConnection(function (err, connection) {
+        var _query = "INSERT INTO `hurestic`.`swipes` (`empid`, `direction`, `timestamp`, `deviceid`) VALUES ('" + event.empid + "', '" + event.direction.toUpperCase() + "', DATE_ADD(DATE_ADD(UTC_TIMESTAMP(),INTERVAL 5 HOUR), INTERVAL 30 MINUTE), " + event.deviceid + ")";
+        console.log("Query:", _query);
+        connection.query(_query, function (err, rows) {
+            if (err !== null) {
+                console.log("Error:" , err);
+            }
+            connection.release();
+            context.succeed(JSON.stringify(event));
+        });
+    });
+};
+
+exports.handler({
+    "deviceid": 2102,
+    "direction" : "in",
+    "empid" : "B113"
+}, null);
+
+
+
